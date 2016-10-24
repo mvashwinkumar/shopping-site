4ba95296259852edef2d217218cc5a36acfe5a86
mvashwinkumar <mvashwinkumar@gmail.com>
2016-10-24 20:34:53 +0800

    Remove unwanted generated files from ts source code

--- app/product/product.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Product = (function () {
-    function Product(id, sku, name, mageUrl, urlpath, ident, size, designer, kidzdesigner, condition, stylesort, styles, price, oldPrice, status, image, smallImage, secondaryImage, manufacturer, wishlist) {
-    }
-    return Product;
-}());
-exports.Product = Product;
-//# sourceMappingURL=product.js.map
